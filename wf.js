d3c8ecacd0288e77b4a253c53e32c04cf2e73b22
Devon Patey <devon.patey@gmail.com>
2017-06-16 15:51:33 -0600

    added letter boxes and array of words

--- wf.js
+++ wf.js
@@ -5,5 +5,41 @@ $(document).ready(function() {
 		$("#spinArea").html("Spin: $ " + points);
 	});
 
+	function Words() {
+		this.answers_array = [];
+		this.addWord = function(word) {
+			this.answers_array.push(word);
+		};
+
+		this.getWordList = function() {
+			let word_list = [];
+			for (var i = 0; i < this.answers_array.length; i++) {
+				word_list.push(this.answers_array[i].words);
+			}
+			console.log(word_list);
+		};
+	};
+
+	function Word(words) {
+		this.words = words;
+	};
+
+	var wordAnswers = new Words();
+	var devon = new Word;
+	var patey = new Word;
+	var wheel = new Word;
+	var fortune = new Word;
+
+	wordAnswers.addWord(devon);
+	wordAnswers.addWord(patey);
+	wordAnswers.addWord(wheel);
+	wordAnswers.addWord(fortune);
+
+
+	
+	$(".buttons").click(function(keypress){
+
+	});
+
 }); // end doc ready function
 
